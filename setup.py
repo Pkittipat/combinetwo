8b3487b881ca22b969a0d97f3ef8f6ca1aed34fd
Pkittipat <kittipat.phongsak@gmail.com>
2020-02-25 18:18:13 +0700

    adapt gz version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'pkittipat',                   # Type in your name
   author_email = 'kittipat.phongsak@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Pkittipat/combinetwo.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Pkittipat/combinetwo/archive/0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Pkittipat/combinetwo/archive/0.0.2.tar.gz',    # I explain this later on
   keywords = ['CPRINT', 'BPRINT'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
